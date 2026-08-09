e4473d6dec85b8ac24bfb7df0d96a7341ce86017
Srixjan <srijan.chowdhury2020@gmail.com>
2026-08-10 00:50:35 +0530

    finished oops and programing

---
+++ Task_4.3.py
@@ -0,0 +1,24 @@
+class Product:
+    store_name = "Bright Mart"
+    total_products = 0
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+        Product.total_products += 1 
+
+    def display_info(self):
+        print(f"Store: {Product.store_name}")
+        print(f"Product: {self.name}")
+        print(f"Price: ₹{self.price}")
+        print(f"Total Products Created: {Product.total_products}")
+
+p1 = Product("NoteBook", 50)
+p1.store_name="Solo man"
+p1.display_info()
+
+print()
+p2 = Product("Pencil Bag", 250)
+p2.display_info()
+print()
+p3 = Product("Geometry Box", 500)
+p3.display_info()
