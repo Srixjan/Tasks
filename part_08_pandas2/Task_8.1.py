b0c56b6c0e1572488d5711e20822a8fa79096f72
Srixjan <srijan.chowdhury2020@gmail.com>
2026-08-26 01:04:16 +0530

    pandas finished

--- part_08_pandas2/Task_8.1.py
+++ part_08_pandas2/Task_8.1.py
@@ -15,5 +15,32 @@ def groupby_summary(df: pd.DataFrame) -> pd.DataFrame:
 def groupby_multi(df: pd.DataFrame) -> pd.DataFrame:
     return df.groupby(["neighbourhood_group", "room_type"]).agg(average_price=("price", "mean")).reset_index()
 
+def build_borough_lookup() -> pd.DataFrame:
+    borough_data = {
+    "neighbourhood_group": df["neighbourhood_group"].unique(),
+    "borough_population_milltions": [2.6, 1.6, 2.3, 0.5, 1.4],
+    "is_manhattan_adjacent": ['True', 'False', 'True', 'False', 'True']
+    }
+
+    return borough_data
+
+borough_data = build_borough_lookup()
+borough_info = pd.DataFrame(borough_data)
+borough_info
+
+def merge_listings(df: pd.DataFrame, lookup: pd.DataFrame, how:str) -> pd.DataFrame:
+    return df.merge(lookup, on="neighbourhood_group", how=how)
+
+testing = merge_listings(df, borough_info, "left")
+testing
+
+testing.shape[0] == df.shape[0]
+
+borough_info_2 = borough_info.copy()
+borough_info_2 = borough_info_2.drop([0, 1])
+
+testing_2 = merge_listings(df, borough_info_2, "left")
+testing_2.shape[0] == df.shape[0]
+
 print(groupby_multi(df))
 print(groupby_summary(df))
