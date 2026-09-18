1384e7e16e5e70972e06d203d628915497e8e70c
Srixjan <srijan.chowdhury2020@gmail.com>
2026-09-19 00:21:10 +0530

    Refactor CompletionRiskModel: Add training method and logging
    - Introduced a `train` method in the `CompletionRiskModel` class to handle model training.
    - Integrated feature pipeline for preprocessing data before training.
    - Added logging for training completion and accuracy metrics.
    - Updated imports to include necessary modules for training functionality.
    - Cleaned up code structure and added comments for clarity.

--- mini_project_mplads_fund_eda/src/model.py
+++ mini_project_mplads_fund_eda/src/model.py
@@ -1,8 +1,14 @@
 import logging
+
+from sklearn.model_selection import train_test_split
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
+
+from src.risk_target import defined_completion_risk
+
+from src.feature_pipeline import build_feature_pipeline
 
 class UnknownClassifierError(Exception):
     """raise this if user submits model out of model cabinet"""
@@ -45,4 +51,35 @@ class CompletionRiskModel:
             self.classifier = model_class()
 
         logger.info(f"Successfully initialized classifier, {name}")
+
+        ## this shit works till now, please dont fuck it up!!
+
+    def train(self, df):
+        rand_stat = self.config["random_state"]
         
+        target_df = defined_completion_risk(df)
+        y = target_df["at_risk"]
+        X = target_df.drop(columns=["at_risk"])
+
+        pipeline = build_feature_pipeline()
+
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=rand_stat, stratify=y
+        )
+
+        X_train_trans = pipeline.fit_transform(X_train)
+        X_test_trans = pipeline.transform(X_test)
+
+        self.pipeline = pipeline
+
+        self.classifier.fit(X_train_trans, y_train)
+
+        train_acc = self.classifier.score(X_train_trans, y_train)
+        test_acc = self.classifier.score(X_test_trans ,y_test)
+
+        logging.info(
+            f"Training Complete. Model:{self.config['model_name']}."
+            f"Train accuracy -> {train_acc:.2f%}, Test accuracy -> {test_acc.2f%}"
+        )
+
+        self.is_trained = True
