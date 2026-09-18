2f8560b6dfeaf7b49e758007de4ee04efb474836
Srixjan <srijan.chowdhury2020@gmail.com>
2026-09-19 01:14:23 +0530

    Refactor CompletionRiskModel: Add predict and save methods, enhance logging, and update execution counts in Jupyter notebook

--- mini_project_mplads_fund_eda/src/model.py
+++ mini_project_mplads_fund_eda/src/model.py
@@ -1,4 +1,7 @@
 import logging
+import joblib
+import pandas as pd
+import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.base import BaseEstimator, ClassifierMixin
@@ -9,6 +12,10 @@ from sklearn.neighbors import KNeighborsClassifier
 from src.risk_target import defined_completion_risk
 
 from src.feature_pipeline import build_feature_pipeline
+
+class ModelNotTrainedError(Exception):
+    """raise this if user forgets to trains, and goes directly to predict"""
+    pass
 
 class UnknownClassifierError(Exception):
     """raise this if user submits model out of model cabinet"""
@@ -70,7 +77,7 @@ class CompletionRiskModel:
         X_train_trans = pipeline.fit_transform(X_train)
         X_test_trans = pipeline.transform(X_test)
 
-        self.pipeline = pipeline
+        self.pipeline = pipeline # backup to class.
 
         self.classifier.fit(X_train_trans, y_train)
 
@@ -79,7 +86,48 @@ class CompletionRiskModel:
 
         logging.info(
             f"Training Complete. Model:{self.config['model_name']}."
-            f"Train accuracy -> {train_acc:.2f%}, Test accuracy -> {test_acc:.2f%}"
+            f"Train accuracy -> {train_acc:.2f}, Test accuracy -> {test_acc:.2f}"
         )
 
         self.is_trained = True
+
+    def predict(self, X_fresh):
+        if not self.is_trained or self.pipeline is None:
+            raise ModelNotTrainedError(f"Model has not been trained yet....")
+
+        X_fresh_trans = self.pipeline.transform(X_fresh)
+
+        predictions = self.classifier.predict(X_fresh_trans)
+
+        return list(predictions)
+
+    def save(self, filepath):
+        if not self.is_trained:
+            raise ModelNotTrainedError(f"Train and Predict your model first!!!!")
+
+        artifact = {
+            "config": self.config,
+            "pipeline": self.pipeline,
+            "classifier": self.classifier
+        }
+
+        joblib.dump(artifact, filepath)
+        logger.info(f"Model + pipeline + config saved successfully to {filepath}")
+
+    @classmethod
+    def load(cls, filepath):
+        artifact = joblib.load(filepath)
+        saved_config = artifact["config"]
+
+        instance = cls(
+            model=saved_config["model_name"],
+            random_state=saved_config["random_state"]
+        )
+
+        instance.config = saved_config
+        instance.pipeline = artifact["pipeline"]
+        instance.classifier = artifact["classifier"]
+        instance.is_trained = True
+
+        logger.info(f"Model artifact successfully restored from {filepath}. Ready to predict.")
+        return instance
